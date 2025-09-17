75eebbf60f7ebbd08550b72bb0314e4c5d75869b
Omar Guendeli <>
2025-09-17 19:10:41 +0400

    init setup

---
+++ Assets/QuantumUser/Simulation/GameplaySystemsGroup.cs
@@ -0,0 +1,12 @@
+﻿namespace Quantum
+{
+	using UnityEngine.Scripting;
+	[Preserve]
+	public class GameplaySystemsGroup : SystemMainThreadGroup
+	{
+		public GameplaySystemsGroup(params SystemBase[] children) : base(children) { }
+		public GameplaySystemsGroup(string update, params SystemMainThread[] children) : base(update, children)
+		{
+		}
+	}
+}
