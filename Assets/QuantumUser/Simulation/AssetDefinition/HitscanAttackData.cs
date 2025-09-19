ee4b1513f740b749bd49a50d8af2502174afcec4
Omar Guendeli <>
2025-09-19 14:47:24 +0400

    HitScan Attack logic and data assets

---
+++ Assets/QuantumUser/Simulation/AssetDefinition/HitscanAttackData.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Deterministic;
+using System;
+
+namespace Quantum
+{
+    // Base class for any attack which shoots a simple line cast with no extra logic other than causing damage
+
+    [System.Serializable]
+    public unsafe abstract partial class HitscanAttackData : AttackData
+    {
+        public FP Distance;     // For LineCast attacks
+        public override void OnUpdate(Frame frame, EntityRef attackEntity, Attack* attack)
+        {
+            base.OnUpdate(frame, attackEntity, attack);
+            bool wasDisabled = CheckHit(frame, attackEntity, attack);
+
+            if(wasDisabled == true)
+            {
+                return;
+            }
+        }
+
+        private bool CheckHit(Frame frame, EntityRef attackEntity, Attack* attack) {
+            Transform2D* attackTransform = frame.Unsafe.GetPointer<Transform2D>(attackEntity);
+            var hits = PhysicsHelper.HitScanCollision(frame, attackTransform, Distance);
+            if (hits.Count == 0)
+            {
+                return false;
+            }
+
+            EntityRef targetEntity = CheckHits(frame, hits, attackEntity, attack, attackTransform, out bool wasDisabled);
+            if (targetEntity != default)
+            {
+                OnApplyEffect(frame, attack->Source, targetEntity);
+            }
+
+            return wasDisabled;
+        }
+        
+    }
+}
---
+++ Assets/QuantumUser/Simulation/AssetDefinition/SniperBasicAttackData.cs
@@ -0,0 +1,9 @@
+﻿namespace Quantum
+{
+    [System.Serializable]
+    public unsafe partial class SniperBasicAttackData : HitscanAttackData
+    {
+        // The Sniper basic attack currently doesn't need any extra logic. Only it's parent class logic is necessary
+        // We still created this asset as to make it class specific, but that's not mandatory
+    }
+}
