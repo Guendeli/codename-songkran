297c3c8dd23de5a6f79b5a8108759fa1bee8fe5c
Omar Guendeli <>
2025-10-24 10:49:52 +0400

    WIP: Survivor branch

--- Assets/QuantumUser/Simulation/System/CharacterSkillCreationSystem.cs
+++ Assets/QuantumUser/Simulation/System/CharacterSkillCreationSystem.cs
@@ -37,7 +37,7 @@ namespace Quantum
         entity,
         entityPos,
         movementData,
-        characterAttacks->BasicSkillData);
+        characterAttacks->BasicSkillData, filter);
 
       ApplyAttackInput(frame,
         input,
@@ -45,23 +45,42 @@ namespace Quantum
         entity,
         entityPos,
         movementData,
-        characterAttacks->SpecialSkillData);
+        characterAttacks->SpecialSkillData, filter);
     }
 
-    private void ApplyAttackInput(Frame frame, QuantumDemoInputTopDown input, Button button, EntityRef entity, FPVector2 entityPos, MovementData* movementData, AssetRef<SkillData> dataRef)
+    private void ApplyAttackInput(Frame frame, QuantumDemoInputTopDown input, Button button, EntityRef entity, FPVector2 entityPos, MovementData* movementData, AssetRef<SkillData> dataRef, Filter filter)
     {
-      bool actionReleased = button.WasReleased;
+      SkillData data = frame.FindAsset<SkillData>(dataRef.Id);
+      if(filter.InputContainer->TimeSinceAutoAim > 0)
+        filter.InputContainer->TimeSinceAutoAim -= frame.DeltaTime;
+      
+      bool actionReleased = button.WasReleased || CanAutoAim(frame, entity, data, filter.InputContainer);
 
       if (actionReleased == true && movementData->IsOnAttackLock == false)
       {
-        SkillData data = frame.FindAsset<SkillData>(dataRef.Id);
         EAttributeType costType = data.CostType;
         
         FP energyAttribute = AttributesHelper.GetCurrentValue(frame, entity, costType);
         if (energyAttribute >= data.Cost)
         {
+          FPVector2 aimDirection = input.AimDirection;
+          if (CanAutoAim(frame, entity, data, filter.InputContainer))
+          {
+            EntityRef closestEnemy = default;
+            EnemyPositionsHelper.TryGetClosestCharacter(frame, entity,
+              data.AutoAimRadius, checkLineSight: true, ignoreSameTeam: true, out closestEnemy);
+            if (closestEnemy == default)
+              return;
+            
+            Transform2D* targetTransform = frame.Unsafe.GetPointer<Transform2D>(closestEnemy);
+            
+            aimDirection = (targetTransform->Position - entityPos);
+            Log.Debug(string.Format("Casting auto-aim towards {0} at direction {1}", closestEnemy.Index, aimDirection));
+          }
+          
+          
           movementData->DirectionTimer = data.RotationLockDuration;
-          frame.Signals.OnCreateSkill(entity, entityPos, data, input.AimDirection);
+          frame.Signals.OnCreateSkill(entity, entityPos, data, aimDirection);
           movementData->MovementTimer = data.MovementLockDuration;
         }
       }
@@ -75,6 +94,45 @@ namespace Quantum
       {
         movementData->MovementTimer -= frame.DeltaTime;
       }
+    }
+
+    private bool CanAutoAim(Frame frame, EntityRef source, SkillData skillData, InputContainer* inputContainer)
+    {
+      Bot* bot = frame.Unsafe.GetPointer<Bot>(source);
+      if (bot->IsActive)
+        return false;
+
+      if (skillData.AutoAimCheckSight && inputContainer->TimeSinceAutoAim <= 0)
+      {
+        return true;
+      }
+      
+      return false;
+    }
+    private bool GetClosestEnemyDirection(Frame frame, EntityRef source, FPVector2 sourcePos, FP radius)
+    {
+      // TeamInfo sourceTeamInfo = frame.Get<TeamInfo>(source);
+      // Transform2D* attackTransform = frame.Unsafe.GetPointer<Transform2D>(source);
+      //
+      // var layerMask = frame.Layers.GetLayerMask(AIConstants.LAYER_CHARACTER);
+      // var hits = PhysicsHelper.OverlapShape(frame, attackTransform, layerMask, );
+      // for (int i = 0; i < hits.Count; i++)
+      // {
+      //   EntityRef target = hits[i].Entity;
+      //   TeamInfo* teamInfo = frame.Unsafe.GetPointer<TeamInfo>(target);
+      //
+      //   if (HitAlies == false && sourceTeamInfo.Index == teamInfo->Index)
+      //   {
+      //     continue;
+      //   }
+      //
+      //   if (IgnoreOwner == true && target.Equals(attack.Source) == true)
+      //   {
+      //     continue;
+      //   }
+      //   OnApplyEffectAll(frame, attack.Source, target);
+
+      return false;
     }
   }
 }
