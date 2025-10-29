0913db6d7d6b33871f19ab069992cfdca1f3b8df
Omar Guendeli <>
2025-10-29 16:29:05 +0400

    Payload game mode AI

---
+++ Assets/QuantumUser/Simulation/AI/Payload/FindPayload.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Deterministic;
+
+namespace Quantum
+{
+    [System.Serializable]
+    public unsafe class FindPayload : AIAction
+    {
+        public AIBlackboardValueKey ObjectivePoint;
+
+        public override void Execute(Frame frame, EntityRef entity, ref AIContext aiContext)
+        {
+            AIBlackboardComponent* blackboard = frame.Unsafe.GetPointer<AIBlackboardComponent>(entity);
+
+            FPVector2 myPosition = frame.Get<Transform2D>(entity).Position;
+
+            var payloadEntity = frame.Global->PayloadEntity;
+            if (!payloadEntity.IsValid)
+            {
+                return;
+            }
+            Transform2D* payloadTransform = frame.Unsafe.GetPointer<Transform2D>(payloadEntity);
+
+            if (payloadEntity == default)
+                return;
+            
+            
+            blackboard->Set(frame, ObjectivePoint.Key, payloadTransform->Position);
+        }
+    }
+}
