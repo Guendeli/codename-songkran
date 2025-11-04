0a4268b4eec625413b5284ef28978dc011d2135e
Omar Guendeli <>
2025-11-04 18:54:13 +0700

    nullptr guardrail

--- Assets/QuantumUser/Simulation/AssetDefinition/AI/BotSteeringData.cs
+++ Assets/QuantumUser/Simulation/AssetDefinition/AI/BotSteeringData.cs
@@ -296,6 +296,8 @@ namespace Quantum
 		{
 			FPVector2 runawayDirection = default;
 
+			if(entry->Entity.IsValid == false)
+				return default;
 			// Run to the perpendicular direction considering the attack
 			Transform2D attackerTransform = frame.Get<Transform2D>(entry->Entity);
 			FPVector2 dir = frame.Get<Transform2D>(agent).Position - frame.Get<Transform2D>(entry->Entity).Position;
